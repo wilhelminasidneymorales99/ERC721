8504558f568225b4ec250a94e6c3ff7d12cebfc2
wilhelminasidneymorales99 <wilhelminasidneymorales99@hotmail.com>
2024-08-26 03:09:28 +0000

    Initialised

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("jj","jj")
+        ERC721("uu","uu")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
